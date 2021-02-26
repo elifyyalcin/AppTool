9268010e243c912fcda0b5fdf5465f93d0181adb
elifyyalcin <46829670+elifyyalcin@users.noreply.github.com>
2021-02-26 03:43:22 +0300

    Update todo.dart

--- lib/models/todo.dart
+++ lib/models/todo.dart
@@ -6,7 +6,6 @@ class Todo {
   Todo({this.name, this.description});
   Todo.withId({this.id, this.name, this.description});
 
-  //Kaynak videodan yararlanıldı.(BTK flutter eğitimi)
   Map<String, dynamic> toMap() {
     var map = Map<String, dynamic>();
     map["name"] = name;
@@ -17,7 +16,6 @@ class Todo {
     return map;
   }
 
-  //Kaynak videodan yararlanıldı.(BTK flutter eğitimi)
   Todo.fromObject(dynamic o) {
     this.id = o["id"];
     this.name = o["name"];
